e9a7250c20363fcbc87a959d9e5cb7949527db74
dadanmdahlan <dadan.muhammad13@gmail.com>
2021-04-30 10:10:42 +0700

    belajar awal class

--- dicoding/belajar.js
+++ dicoding/belajar.js
@@ -1,2 +1,27 @@
+// cara 1
+// function 
 
+function Mail() {
+    this.from = 'pengirim@dicoding.com';
+    this.sendMessage = function (msg, to){
+        console.log('you send: ',msg,' to ',to, 'from', this.from);
+    };
+    
+}
 
+const mail1 = new Mail ();
+mail1.sendMessage('hallo','penerima@dicoding.com');
+
+// cara 2
+// class
+
+class Mail3 {
+    from = 'pengirim@dicoding.com';
+    sendMessage = function (msg, to){
+        console.log('you send: ',msg,' to ',to, 'from', this.from);
+    };
+    
+}
+
+const mail4 = new Mail3 ();
+mail4.sendMessage('hallo','penerima@dicoding.com');
